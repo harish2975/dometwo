25f898bd12d1b7fba62c4b1434979c82c38cd5d6
harish2975 <harishraghav2975@gmail.com>
2025-01-01 23:12:24 +0530

    Update index.html

--- index.php
+++ index.php
@@ -11,5 +11,23 @@
         <h1>Sign in to X</h1>
         <form>
             <button type="button">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Goo
-vji-qfoo-pxu
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Google">
+                Sign in with Google
+            </button>
+            <button type="button">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt="Apple">
+                Sign in with Apple
+            </button>
+            <div class="separator">
+                <div></div>
+                <span>or</span>
+                <div></div>
+            </div>
+            <input type="text" placeholder="Phone, email, or username">
+            <button type="submit">Next</button>
+            <a href="#" class="link">Forgot password?</a>
+        </form>
+        <a href="#" class="link">Don't have an account? Sign up</a>
+    </div>
+</body>
+</html>
